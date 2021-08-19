ed01da59c71169df59c1538d1ab89578c0431da4
SitiJalilahMustaffa <sitijalilah5196@gmail.com>
2021-08-19 21:49:42 +0800

    SOVS

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> Smart Online Voting System Login</title>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="contact-form">
+		<img alt="" class="avatar" src="https://i.pinimg.com/736x/3f/94/70/3f9470b34a8e3f526dbdb022f9f19cf7.jpg">
+		<h2>Login to SOVS</h2>
+		<form action="">
+			<p>Email</p><input placeholder="Enter Email" type="email">
+			<p>Passwrod</p><input placeholder="Enter Password" type="password"> <input type="submit" value="Sign in">
+			<p><input type="checkbox">Remember Me</p>
+			<p><a class="small" href="forgot-password.php">Forgot Password?</a></p>
+			
+		</form>
+	</div>
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,84 @@
+ <!-- Sidebar -->
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon rotate-n-15">
+        <i class="fas fa-vote-yea"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">SOVS</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item active">
+    <a class="nav-link" href="index.php">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+</li>
+
+ <!-- Nav Item - Report -->
+<li class="nav-item">
+<a class="nav-link" href="reports.php">
+    <i class="fas fa-fw fa-chart-area"></i>
+    <span>Report</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Manage
+</div>
+
+<!-- Nav Item - Student -->
+<li class="nav-item">
+<a class="nav-link" href="students.php">
+<i class="fas fa-user-graduate"></i>
+    <span>Student</span></a>
+</li>
+
+<!-- Nav Item - Candidate -->
+<li class="nav-item">
+<a class="nav-link" href="candidates.php">
+<i class="far fa-address-card"></i>
+    <span>Candidate</span></a>
+</li>
+
+
+
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Settings
+</div>
+
+
+
+<!-- Nav Item - Election -->
+<li class="nav-item">
+    <a class="nav-link" href="elections.php">
+    <i class="fas fa-cogs"></i>
+        <span>Election</span></a>
+</li>
+
+
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+
+
+</ul>
+<!-- End of Sidebar -->
